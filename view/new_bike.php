f39990041efb72d31616775da946fe211ffb5a86
Joana Freitas <joanafreitas13@gmail.com>
2023-05-13 15:50:32 +0200

    Design

--- view/new_bike.php
+++ view/new_bike.php
@@ -9,6 +9,7 @@ $pwd = 'pillabike';
 $db = new PDO('mysql:host=localhost;dbname=pillaBike', $user, $pwd);
 ?>
 
+
     <section class="h-100 gradient-form" style="background-color: #eee;">
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
